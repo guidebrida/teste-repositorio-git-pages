d076db9b2d46bd824e82129c5854cb50a966f8d0
guilh <guilhermedbdb@gmail.com>
2025-11-17 11:03:38 -0300

    commit

---
+++ js/script_pag_modalidades.js
@@ -0,0 +1,49 @@
+/*funcionalidade da barra de navegação para dispositivos mobile*/
+class MobileNavbar{
+    constructor(mobileMenu, navList, navLinks){
+        this.MobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.MobileMenu.classList.toggle(this.activeClass)
+        this.animateLinks();
+    }
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ?(link.style.animation = "")
+            :(link.style.animation = `navLinkFade 0.5s ease forwards ${index/ 7 + 0.3}s`)
+            
+        })
+    }
+    addClickEvent(){
+        this.MobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init(){
+        if(this.MobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+const mobileNavbar = new MobileNavbar(
+    "#Mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+)
+mobileNavbar.init();
+
+
+
+document.querySelectorAll("p.hora").forEach(p => {
+    p.style.cursor = "pointer"; // só para indicar que é clicável
+    p.addEventListener("click", () => {
+        window.location.href = "../html/horarios.html"; 
+    });
+});
